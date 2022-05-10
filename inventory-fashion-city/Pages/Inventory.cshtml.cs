defc94bcbbd9064611dd6022d8242cc47f0f7dc7
vasga-floze <gabriela.floze@gmail.com>
2022-05-10 01:25:58 -0600

    initial system interface

---
+++ inventory-fashion-city/Pages/Inventory.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inventory_fashion_city.Pages
+{
+    public class InventoryModel : PageModel
+    {
+        private readonly ILogger<InventoryModel> _logger;
+
+        public InventoryModel(ILogger<InventoryModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ inventory-fashion-city/Pages/Login.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inventory_fashion_city.Pages
+{
+    public class LoginModel : PageModel
+    {
+        private readonly ILogger<LoginModel> _logger;
+
+        public LoginModel(ILogger<LoginModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
